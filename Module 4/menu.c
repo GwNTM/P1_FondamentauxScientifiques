72984ee64e5d616e546f66f60952dd88282c8399
Mmouky <33157839+Mmouky@users.noreply.github.com>
2017-11-16 17:00:18 +0100

    Module 3.4 - changement 2

--- Module 4/menu.c
+++ Module 4/menu.c
@@ -4,25 +4,24 @@
 #include "actions.h"
 
 void printMenu(){
-    printf(" ___________________________________________________________________________________ \n");
-    printf("|                                                                                   |\n");
-    printf("|                                        MENU                                       |\n");
-    printf("|___________________________________________________________________________________|\n");
-    printf("|                                                                                   |\n");
-    printf("|    1 - Afficher les donnees dans l'ordre du fichier .csv                          |\n");
-    printf("|    2 - Afficher les donnees dans l'ordre inverse du fichier .csv                  |\n");
-    printf("|    3 - Afficher les donnees en ordre croissant (selon le pouls)                   |\n");
-    printf("|    4 - Afficher les donnees en ordre decroissant (selon le pouls)                 |\n");
-    printf("|    5 - Rechercher et afficher les donnees pour un temps particulier               |\n");
-    printf("|    6 - Afficher la moyenne de pouls dans une plage de temps donnee                |\n");
-    printf("|    7 - Afficher le nombre de lignes de donnees actuellement en memoire            |\n");
-    printf("|    8 - Rechercher et afficher les max/min de pouls                                |\n");
-    printf("|                                                                                   |\n");
-    printf("|    0 - Quitter l'application                                                      |\n");
-    printf("|___________________________________________________________________________________|\n");
+    printf("     ___________________________________________________________________________________ \n");
+    printf("    |                                                                                   |\n");
+    printf("    |                                        MENU                                       |\n");
+    printf("    |___________________________________________________________________________________|\n");
+    printf("    |                                                                                   |\n");
+    printf("    |    1 - Afficher les donnees dans l'ordre du fichier .csv                          |\n");
+    printf("    |    2 - Afficher les donnees dans l'ordre inverse du fichier .csv                  |\n");
+    printf("    |    3 - Afficher les donnees en ordre croissant (selon le pouls)                   |\n");
+    printf("    |    4 - Afficher les donnees en ordre decroissant (selon le pouls)                 |\n");
+    printf("    |    5 - Rechercher et afficher les donnees pour un temps particulier               |\n");
+    printf("    |    6 - Afficher la moyenne de pouls dans une plage de temps donnee                |\n");
+    printf("    |    7 - Afficher le nombre de lignes de donnees actuellement en memoire            |\n");
+    printf("    |    8 - Rechercher et afficher les max/min de pouls                                |\n");
+    printf("    |                                                                                   |\n");
+    printf("    |    0 - Quitter l'application                                                      |\n");
+    printf("    |___________________________________________________________________________________|\n");
     printf("\n");
 }
-
 int choseAction(){
     int action = 0;
     do{
